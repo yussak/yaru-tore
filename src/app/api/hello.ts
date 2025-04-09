0d460afecaabea8aba6a624dba1fd6732983832e
sakuraba-yu <sakuraba-yu@ibjapan.jp>
2025-04-09 17:28:24 +0900

    環境構築

---
+++ src/app/api/hello.ts
@@ -0,0 +1,8 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<{ message: string }>
+) {
+  res.status(200).json({ message: 'Hello from Node.js backend!' });
+}
